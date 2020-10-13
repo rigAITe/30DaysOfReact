18097a0e66f2debec555029a466c5557d6dce25c
Tunmishe Israel <aitboy18@gmail.com>
2020-10-14 00:34:34 +0100

    Destruction Props

--- src/index.js
+++ src/index.js
@@ -6,126 +6,206 @@ import react_logo from './img/react_logo.png'
 import css_logo from './img/css_logo.png'
 import asa from './img/asabeneh.jpg'
 
-
-const userDetail = {
-  firstName : 'ASABENEH',
-  lastName : 'YETAYEH',
-  profession : 'Senior Developer',
-  country : 'Finland',
-  skills : [
-    'HTML', 
-    'CSS', 
-    'Sass', 
-    'JS', 
-    'React', 
-    'Redux', 
-    'Node', 
-    'MongoDB', 
-    'MYSQL', 
-    'D3.JS', 
-    'Gatsby', 
-    'Docker', 
-    'Heroku', 
-    'Git',
-    'Python',
-    'Flask',
-    'Django', 
-    'NumPy', 
-    'Pandas', 
-    'Data Analysis'
-  ],
-  date : 'October, 9 2020'
+const Logo = ({data: {
+    heading,
+    html,
+    css,
+    react
+}
+   }) => {
+    return (
+        <div className = "header">
+            <h2 className = 'head'>{heading}</h2>
+            <div className = 'logo'>
+                <img src={html} alt=''/>
+                <img src={css} alt=''/>
+                <img src={react} alt=''/>
+            </div>
+        </div>
+    )
 }
 
-/* ------- FRONT END IMAGE -----*/
-const Heading = () => (
-  <div className = "header">
-    <h2 className = 'head'>Front End Technologies</h2>
-    <div className = 'logo'>
-      <img src = {css_logo} alt = ''/>
-      <img src = {react_logo} alt = '' />
-      <img src = {html5} alt = ''/>
-    </div>
-  </div>
-)
 
-/* ------- USER CARD -----*/
-
-const listFormatted = userDetail.skills.map( skill => <li>{skill}</li>)
-
-const UserCard = () => (
-  <div className = 'userCard'>
-    <div className = 'userInfo'>
-      <img className= 'userImage' src = {asa} alt = ''/>
-      <h3 className = 'username'>{userDetail.firstName} {userDetail.lastName}</h3>
-      <small>{userDetail.profession}, {userDetail.country}</small>
-      <div>
-        <h3>SKILL</h3>
-        <ul className = 'lists'>{listFormatted}</ul>
-      </div>
-      <small> Joined on {userDetail.date}</small>
-    </div>
-  </div>
-)
-
-/* ------- SUBSCRIPTION FORM -----*/
-
-const Forms = () => (
-  <div className = "subscribe">
-    <h2>SUBSCRIBE</h2>
-    <p>Sign up with your email address to reaceive news and updates</p>
-    <div className = "input">
-      <input placeholder = "First name"/>
-      <input placeholder = "Last name"/>
-      <input placeholder = "Email"/>
-    </div>
-    <button>Subscribe</button>
-  </div>
-)
-
-const hexadecimal = () => {
-  let str = '0123456789abcdef'
-  let color = ''
-
-  for ( let i = 0; i < 6; i++){
-    let random = Math.floor(Math.random() * str.length );
-    color += str[random]
-  }
-  return '#' + color
-} 
+const UserCard = ({ user: {
+    firstName,
+    lastName,
+    image
+},
+    }) => {
+    return (
+        <div>
+            <img className= 'userImage' src = {image} alt = ''/>
+            <h3 className = 'username'>{firstName} {lastName}</h3>
+        </div>
+    )
+}
 
 
-
-const Hexadecimal = () => (
-  <ul className = 'hexa'>
-    <li id = "#518cef">#518cef</li>
-    <li id = "#3b10c4">#3b10c4</li>
-    <li id = "#9aede6">#9aede6</li>
-    <li id = "#8ee763">#8ee763</li>
-    <li id = "#a30dd0">#a30dd0</li>
-  </ul>
-)
-
-let firstColor = '#518cef'
-const first = document.getElementById('#518cef');
+const SkillList = ({list}) => {
+    const TechList = list.map( skill => <li key={skill}>{skill}</li>)
+    return TechList;
+}
 
 
-while (firstColor !== hexadecimal() ){
-    first.style.backgroundColor = "#518cef"
-  }
+const User = ({ userDetails: {
+    profession,
+    country,
+    skillsHeading,
+    joined,
+    date : {
+        month,
+        day,
+        year
+    }
+},
+    }) => {
+    const user = {
+        firstName : 'ASABENEH',
+        lastName : 'YETAYEH', 
+        image: asa,
+    }
 
+    const skills =  [
+        'HTML', 
+        'CSS', 
+        'Sass', 
+        'JS', 
+        'React', 
+        'Redux', 
+        'Node', 
+        'MongoDB', 
+        'MYSQL', 
+        'D3.JS', 
+        'Gatsby', 
+        'Docker', 
+        'Heroku', 
+        'Git',
+        'Python',
+        'Flask',
+        'Django', 
+        'NumPy', 
+        'Pandas', 
+        'Data Analysis'
+        ]
 
+    // const list = skills.map( skill => <li key={skill}>{skill}</li>)
+    
+    return ( 
+        <div className = 'userCard'>
+            <div className = 'userInfo'>
+                <UserCard user = {user}/>
+                <small>{profession}, {country}</small>
+                <div>
+                    <h3>{skillsHeading}</h3>
+                    <ul className = 'lists'><SkillList list = {skills} /></ul>
+                </div>
+                <small>{joined} {month} {day}, {year}</small>
+            </div>
+        </div>
+    )
+}
 
-const App = () => (
-  <div>
-    <Heading/>
-    <UserCard/>
-    <Forms/>
-    <Hexadecimal/>
-  </div>
-)
+const Input = ({placeholder}) => {
+    return (
+        <input placeholder = {placeholder}/>
+    )
+}
 
+const Button = ({button}) => {
+    return (
+    <button>{button}</button>
+    )
+}
 
+const Subscribe = ({heading, info }) => {
+    const content = 'Subscribe'
+    const place1 = 'First name'
+    const place2 = 'Last name'
+    const place3 = 'Email'
+    return (
+        <div className = "subscribe">
+            <h2>{heading}</h2>
+            <p>{info}</p>
+            <div className = "input">
+                <Input placeholder= {place1}/>
+                <Input placeholder= {place2}/>
+                <Input placeholder= {place3}/>
+            </div>
+            <Button button = {content}/>
+        </div>
+    )
+}
+
+const App = () => {
+    const data = {
+        heading : 'Front End Development',
+        html : html5,
+        css: css_logo,
+        react : react_logo
+    }
+
+    const time = new Date();
+    
+        // const skillsFormatted = skills.map( skill => <li key ='skill'>{skill}</li>)
+        const month = [
+            'January',
+            'Febuary',
+            'March',
+            'April',
+            'May', 
+            'June',
+            'July', 
+            'August', 
+            'September',
+            'October',
+            'November',
+            'December'
+        ]
+
+    const userDetails = {
+        profession: 'Senior Developer',
+        country: 'Finland', 
+        skillsHeading: 'SKILLS',
+        // skilll: skillsFormatted,
+        joined: 'Joined',
+        date: { 
+            month: month[time.getMonth()].slice(0,3),
+            day: time.getDay(),
+            year: time.getFullYear()
+        }
+    }
+
+    const heading = 'Subscribe'
+    const info = 'Sign up with your Email Address to receive news and update'
+
+    return (
+        <div>
+            <Logo data = {data}/>
+            <User userDetails = {userDetails}/>
+            <Subscribe heading={heading} info={info}/>
+        </div>
+    )
+}
 
 const rootElement = document.getElementById('root')
-ReactDOM.render( <App/>,rootElement)
+ReactDOM.render(<App/>, rootElement)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
